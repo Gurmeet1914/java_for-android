9cf7e3f69863a596c5fe42fedf54d62e761b7103
monak-stack <59800900+monak-stack@users.noreply.github.com>
2020-06-06 10:24:39 +0530

    Add files via upload

--- Logic.java
+++ Logic.java
@@ -1,36 +1,46 @@
-package mooc.vandy.java4android.birthdayprob.logic;
+package mooc.vandy.java4android.shapes.logic;
 
-import mooc.vandy.java4android.birthdayprob.ui.OutputInterface;
+import mooc.vandy.java4android.shapes.ui.OutputInterface;
 import java.util.*;
-import java.util.Random;
+import java.lang.Math.*;
+
 /**
  * This is where the logic of this App is centralized for this assignment.
  * <p>
- * The assignments are designed this way to simplify your early Android interactions.
- * Designing the assignments this way allows you to first learn key 'Java' features without
- * having to beforehand learn the complexities of Android.
+ * The assignments are designed this way to simplify your early
+ * Android interactions.  Designing the assignments this way allows
+ * you to first learn key 'Java' features without having to beforehand
+ * learn the complexities of Android.
  *
  */
-public class Logic<seed>
+public class Logic 
        implements LogicInterface {
     /**
      * This is a String to be used in Logging (if/when you decide you
      * need it for debugging).
      */
-    public static final String TAG =
+    public static final String TAG = 
         Logic.class.getName();
 
-    /**
+    /*
      * This is the variable that stores our OutputInterface instance.
      * <p>
-     * This is how we will interact with the User Interface
-     * [MainActivity.java].
+     * This is how we will interact with the User Interface [MainActivity.java].
      * <p>
-     * It is called 'mOut' because it is where we 'out-put' our
+     * It is called 'out' because it is where we 'out-put' our
      * results. (It is also the 'in-put' from where we get values
-     * from, but it only needs 1 name, and 'mOut' is good enough).
-    */
-    OutputInterface mOut;
+     * from, but it only needs 1 name, and 'out' is good enough)
+     */
+    private OutputInterface mOut;
+
+    /**
+     * These are the numeric values that you will receive from the
+     * User Interface and use in your calculations.
+     */
+    private static double mRadius = 0;
+    private static double mLength = 0;
+    private static double mWidth = 0;
+    private static double mHeight = 0;
 
     /**
      * This is the constructor of this class.
@@ -44,54 +54,149 @@ public class Logic<seed>
 
     /**
      * This is the method that will (eventually) get called when the
-     * on-screen button labelled 'Process...' is pressed.
+     * on-screen button labeled 'Process...' is pressed.
      */
+    @Override
     public void process() {
-        int groupSize = mOut.getSize();
-        int simulationCount = mOut.getCount();
+        // Get which calculation should be computed.  Do not worry
+        // about the specifics of this right now.
+        Shapes shapeForCalculations = mOut.getShape();
 
-        if (groupSize < 2 || groupSize > 365) {
-            mOut.makeAlertToast("Group Size must be in the range 2-365.");
-            return;
+        // Store the values returned by the User Interface.
+        mLength = mOut.getLength();
+        mWidth = mOut.getWidth();
+        mHeight = mOut.getHeight();
+        mRadius = mOut.getRadius();
+
+        // Determine which calculation to process right now.  Again,
+        // do not worry about the specifics of how this works for now.
+        switch (shapeForCalculations) {
+            case Box:
+                mOut.print("A " 
+                           + mLength 
+                           + " by " 
+                           + mWidth 
+                           + " by " 
+                           + mHeight 
+                           + " box has a volume of: ");
+                mOut.println("" 
+                             + String.format("%.2f",
+                                             boxVolume(mLength, mWidth, mHeight)));
+                mOut.println("");
+
+                mOut.print("A " 
+                           + mLength 
+                           + " by " 
+                           + mWidth 
+                           + " by " 
+                           + mHeight 
+                           + " box has a surface area of: ");
+                mOut.println("" 
+                             + String.format("%.2f",
+                                             boxSurfaceArea(mLength, mWidth, mHeight)));
+                mOut.println("");
+                // If you are paying attention, you will notice that
+                // there is no 'break;' here like there is in other
+                // places, meaning that if 'Box' was selected, it will
+                // run the two sets of print statements above and the
+                // two statements below until the 'break;' statement.
+            case Rectangle:
+                mOut.print("A " 
+                           + mLength 
+                           + " by " 
+                           + mWidth 
+                           + " rectangle has a perimeter of: ");
+                mOut.println("" + String.format("%.2f", 
+                                                rectanglePerimeter(mLength, mWidth)));
+                mOut.println("");
+
+                mOut.print("A " 
+                           + mLength 
+                           + " by " 
+                           + mWidth 
+                           + " rectangle has area of: ");
+                mOut.println("" + String.format("%.2f", rectangleArea(mLength, mWidth)));
+                mOut.println("");
+                break;
+            case Sphere:
+                mOut.print("A sphere with radius " + mRadius + " has a volume of: ");
+                mOut.println("" + String.format("%.2f", sphereVolume(mRadius)));
+                mOut.println("");
+
+                mOut.print("A sphere with radius " + mRadius + " has surface area of: ");
+                mOut.println("" + String.format("%.2f", sphereSurfaceArea(mRadius)));
+                mOut.println("");
+                // Same here as with 'Box' above. If 'Sphere' is picked, it will run the
+                // two sets of print statements above and the two below until the 'break;'
+            case Circle:
+                mOut.print("A circle with radius " + mRadius + " has a perimeter of: ");
+                mOut.println("" + String.format("%.2f", circleCircumference(mRadius)));
+                mOut.println("");
+
+                mOut.print("A circle with radius " + mRadius + " has area of: ");
+                mOut.println("" + String.format("%.2f", circleArea(mRadius)) );
+                mOut.println("");
+                break;
+            case Triangle:
+                mOut.print("A right triangle with base " 
+                           + mLength 
+                           + " and height " 
+                           + mWidth + " has a perimeter of: ");
+                mOut.println("" 
+                             + String.format("%.2f", rightTrianglePerimeter(mLength,
+                                                                       mWidth)));
+                mOut.println("");
+
+                mOut.print("A right triangle with base " 
+                           + mLength 
+                           + " and height " 
+                           + mWidth 
+                           + " has area of: ");
+                mOut.println("" 
+                             + String.format("%.2f", rightTriangleArea(mLength,
+                        mWidth)));
+                mOut.println("");
+                break;
+            default:
+                break;
         }
-        if (simulationCount <= 0) {
-            mOut.makeAlertToast("Simulation Count must be positive.");
-            return;
-        }
-
-        double percent = calculate(groupSize, simulationCount);
-
-        // report results
-        mOut.println("For a group of " + groupSize + " people, the percentage");
-        mOut.println("of times that two people share the same birthday is");
-        mOut.println(String.format("%.2f%% of the time.", percent));
-
     }
 
-    public double calculate(int size, int count) {
 
-        // TODO -- add your code here
-        int m = 0;
-        Random random = new Random();
-        for (int i = 0; i < count; ++i)
-        {
-            int arr[]=new int [365];
-            random.setSeed(i+1);
-            for(int j=0;j<size;j++)
-            {
-                int n=random.nextInt(365);
-                arr[n]++;
-                if(arr[n]>=2) {
-                    m++;
-                    break;
-                }
-            }
-        }
-        double percent =  100.0 * m / count;
-
- return(percent);
+    public static double rightTriangleArea(double mLength, double mWidth){
+     return  (mLength*mWidth/2);
     }
-
-
-    // TODO - add your code here
+    public static double circleArea(double mRadius){
+        return(Math.PI*mRadius*mRadius);
+    }
+    public static double rectangleArea(double length, double mWidth)
+    {
+        return (length*mWidth);
+    }
+    public static double boxVolume(double length, double width, double depth)
+    {
+        return(length*width*depth);
+    }
+    public static double boxSurfaceArea(double length, double width,double depth)
+    {
+        return(2*(length*width+width*depth+length*depth));
+    }
+    public static double sphereVolume(double radius)
+    {
+        return((4*Math.PI*radius*radius*radius)/3);
+    }
+    public static double sphereSurfaceArea(double radius)
+    {
+        return(4*Math.PI*radius*radius);
+    }
+    public static double rectanglePerimeter(double length, double width)
+    {
+        return(2*(length+width));
+    }
+    public static double circleCircumference(double radius){
+        return(2*Math.PI*radius);
+    }
+    public static double rightTrianglePerimeter(double base, double height){
+        return(base +height+Math.sqrt(base*base+height*height));
+    }
 }
